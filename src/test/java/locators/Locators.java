5c66c9f41d4f86cadd55bc6d78456327543a6372
AlexAksioma <alexmedqwerty@gmail.com>
2023-12-22 11:54:56 +0200

    added xpath

--- src/test/java/locators/Locators.java
+++ src/test/java/locators/Locators.java
@@ -39,4 +39,49 @@ public class Locators {
 
         driver.quit();
     }
+
+    @Test
+    public void firstXPath() {
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com");
+
+        WebElement elementDiv = driver.findElement(By.xpath("//*[@class='card mt-4 top-card']"));
+        System.out.println("tag name --> " + elementDiv.getTagName());
+
+//        WebElement elementSvg = driver.findElement(
+//                By.xpath("//*[@class='card mt-4 top-card']" +
+//                        "/div/div[@class='avatar mx-auto white']/svg"));
+//        System.out.println("fill value --> "+elementSvg.getAttribute("fill"));
+
+        WebElement elementH5 = driver.findElement(
+                By.xpath("//*[@class='card mt-4 top-card']/div/div[3]/h5"));
+        System.out.println("text h5 --> " + elementH5.getText());
+
+        WebElement elementH5_1 = driver.findElement(
+                By.xpath("//div[@class='category-cards']/div[last()]//h5"));
+        System.out.println("text h5_1 --> " + elementH5_1.getText());
+
+        WebElement elementH5_CSS = driver.findElement(
+                By.cssSelector("div[class='category-cards'] div div div[class='card-body'] h5"));
+        System.out.println("h5 CSS --> " + elementH5_CSS.getText());
+
+        WebElement elementH5_child5 = driver.findElement(
+                By.cssSelector("div[class='category-cards'] :nth-child(5) div[class='card-body'] h5"));
+        System.out.println("h5 child5 --> " + elementH5_child5.getText());
+
+        driver.quit();
+    }
+
+    @Test
+    public void secondXpath(){
+        driver = new ChromeDriver();
+        driver.get("https://demoqa.com/checkbox");
+        WebElement elementInput = driver.findElement(
+                By.xpath("//*[@type='checkbox' and @id='tree-node-desktop']"));
+        System.out.println("names element --> "+elementInput.getTagName());
+
+
+
+        driver.quit();
+    }
 }
